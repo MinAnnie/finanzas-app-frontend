ada78ad161f0945467fdb8f1e8dc994e7d26a657
angie matiz <angiematiz1808@gmail.com>
2024-10-15 15:05:26 -0500

    cleaning workspace

--- src/main/java/org/avmp/springapi/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package org.avmp.springapi.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:messages.properties")
-public class AppConfig {
-}
--- src/main/java/org/avmp/springapi/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package org.avmp.springapi.repositories;
-
-import org.avmp.springapi.entities.Category;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-}
